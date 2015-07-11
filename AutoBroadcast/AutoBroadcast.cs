0205d62f89563e4bbc5dd8aa8f1a2f2771c8ebf0
Zaicon Kiroshu <kzaicon@yahoo.com>
2015-07-10 19:42:11 -0500

    TShock 4.3.3Pre Update

--- AutoBroadcast/AutoBroadcast.cs
+++ AutoBroadcast/AutoBroadcast.cs
@@ -9,7 +9,7 @@ using TShockAPI;
 
 namespace AutoBroadcast
 {
-	[ApiVersion(1, 18)]
+	[ApiVersion(1, 19)]
 	public class AutoBroadcast : TerrariaPlugin
 	{
 		public override string Name { get { return "AutoBroadcast"; } }
